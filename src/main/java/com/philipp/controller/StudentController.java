41e8a9647879ec5ef9619e56212209e311f9cb77
philipp.albers <philipp.albers@senacor.com>
2019-05-21 09:15:19 +0200

    Added Restendpoints

--- src/main/java/com/philipp/controller/StudentController.java
+++ src/main/java/com/philipp/controller/StudentController.java
@@ -3,10 +3,10 @@ package com.philipp.controller;
 import com.philipp.entity.Student;
 import com.philipp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.Collection;
 
 @RestController
@@ -17,6 +17,26 @@ public class StudentController {
 
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents(){
-        return studentService.getAllStudents();
+        return this.studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable("id") int id){
+        return this.studentService.getStudentById(id);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void removeStudentById(@PathVariable("id") int id){
+        this.studentService.removeStudentById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateStudent(@RequestBody  Student student){
+        this.studentService.updateStudent(student);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void insertStudent(@RequestBody  Student student){
+        this.studentService.insertStudent(student);
     }
 }
--- src/main/java/com/philipp/service/StudentService.java
+++ src/main/java/com/philipp/service/StudentService.java
@@ -3,6 +3,7 @@ package com.philipp.service;
 import com.philipp.dao.StudentDao;
 import com.philipp.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -11,9 +12,26 @@ import java.util.Collection;
 public class StudentService {
 
     @Autowired
+    @Qualifier("mongoImpl")
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
         return studentDao.getAllStudents();
+    }
+
+    public Student getStudentById(int id) {
+        return this.studentDao.getStudentById(id);
+    }
+
+    public void removeStudentById(int id){
+        this.studentDao.removeStudentById(id);
+    }
+
+    public void updateStudent(Student student){
+        this.studentDao.updateStudent(student);
+    }
+
+    public void insertStudent(Student student) {
+        this.studentDao.insertStudent(student);
     }
 }
